f22a35fb3d75283f0092b0ea8089e600d397d6e6
Apollo <beanzilla@beanzilla.net>
2023-02-08 15:34:23 -0500

    Stripped out terrible code
       List of things completely removed:
       - meta:string of teamname
       - meta:bool of teamowner
    
       List of things changed:
       - meta:float of teamid changed to meta:int as float can't be exact
       - all functions which get the teamname from the player now
    do so by accessing teamid of the player then looking up the teamname
    from the id
       - better named some functions
       - fixed the loop in remove_player to be lua not python (we don't
    itterate that way in lua)
    
       Still more todo

--- init.lua
+++ init.lua
@@ -17,72 +17,84 @@ mt_teams.colors ={
     white='#FFFFFF',
     yellow='#ffe400',
 }
-mt_teams.teams = {{color=mt_teams.colors.orange,
+mt_teams.teams = {
+    {
+        color=mt_teams.colors.orange,
         owner='singplayer',
         name='base',
         members={},
-        id=mt_teams.teams_num
-    }}
-mt_teams.teams_num = 1
-players = {}
+        id=#mt_teams.teams+1
+    }
+}
 
 function mt_teams.load_teams()
-    mt_teams.teams = minetest.deserialize(storage:get_string('mt_teams:table')) 
+    mt_teams.teams = minetest.deserialize(storage:get_string('mt_teams:table'))
 end
 function mt_teams.save_teams()
-    storage:set_float('mt_teams:is_in_here_somewhere', 1)
     storage:set_string('mt_teams:table', minetest.serialize(mt_teams.teams))
 end
 
-function mt_teams.get_team(player)
+function mt_teams.get_team_id(player)
     local meta = player:get_meta()
-    return meta:get_float("mt_teams:team")
+    return meta:get_int("mt_teams:team")
 end
-function mt_teams.get_teams_name(player)
-    local meta = player:get_meta()
-    return meta:get_string("mt_teams:name")
+
+function mt_teams.get_team_name(player)
+    local team_id = mt_teams.get_team_id(player)
+    return mt_teams.get_team_name_by_id(team_id)
 end
-function mt_teams.get_name_id(id)
+
+function mt_teams.get_team_name_by_id(id)
     return mt_teams.teams[id].name
 end
 function mt_teams.remove_player(player)--removes player and if not there returns false
     local name = player:get_player_name()
-    local team = player:get_meta():get_float('mt_teams:team')
-    i = 1
-    for key, val in mt_teams.teams[team].members do
-        if val==name then mt_teams.teams[team].members.remove(i) return true
-        else i = i+1 end
+    local team = mt_teams.get_team_id(player)
+    local i = 1
+    local found = false
+    for key, val in pairs(mt_teams.teams[team].members) do
+        if val==name then
+            found =true
+            break
+        end
+        i = i+1
+    end
+    if found then
+        table.remove(mt_teams.teams[team].members, i)
+        return true
     end
     return false
 end
+
 function mt_teams.set_team(player,team)
     local meta = player:get_meta()
-    if meta:get_float('mt_teams:team') ~= nil then
+    if meta:get_int('mt_teams:team') ~= nil then
+        -- So that's it, we just remove them... hmm
         local r = mt_teams.remove_player(player)
         if r == false then
-            minetest.chat_send_player(player:get_player_name(), "Not listed in "..mt_teams.get_teams_name(player).."'s members")
+            minetest.chat_send_player(player:get_player_name(), "Not listed in "..mt_teams.get_team_name(player).."'s members")
         --no way to get rid of members in a team so for now... no changing
-        end 
-    end 
+        end
+    end
     if mt_teams.teams[team] ~= nil then
-        meta:set_float('mt_teams:team', team)
-        table_insert(mt_teams.teams[team].members,player:get_player_name())
-        minetest.chat_send_all(string.format("*** %s joined team "..minetest.colorize(mt_teams.teams[meta:get_float("mt_teams:team")].color,mt_teams.teams[team].name),minetest.colorize(mt_teams.teams[meta:get_float("mt_teams:team")].color, player:get_player_name())))
+        meta:set_int('mt_teams:team', team)
+        table_insert(mt_teams.teams[team].members, player:get_player_name())
+        local color = mt_teams.teams[team].color
+        local name = mt_teams.teams[team].name
+        minetest.chat_send_all(string.format("*** %s joined team "..minetest.colorize(color, name), minetest.colorize(color, player:get_player_name())))
     else
         minetest.chat_send_player(player:get_player_name(), "Team doesnt appear to exist")
     end
 end
+
 function mt_teams.create_team(player, name, color)
-    mt_teams.teams_num = mt_teams.teams_num+1
     local meta = player:get_meta()
-    meta:set_float('mt_teams:team', mt_teams.teams_num)
-    meta:set_bool('mt_teams:is_owner', true)
-    meta:set_string('mt_teams:name', name)
-    table.insert_all(mt_teams.teams, 
+    meta:set_int('mt_teams:team', #mt_teams.teams)
+    table.insert_all(mt_teams.teams,
         {color=color,
         owner=player,
         name=name,
-        id=mt_teams.teams_num
+        id=#mt_teams.teams+1
     })
     mt_teams.save_teams()
 end
@@ -90,16 +102,12 @@ end
 minetest.register_on_mods_loaded(function()
     mt_teams.load_teams()
 end)
+
 minetest.register_on_joinplayer(function(player, last_login)
     local meta = player:get_meta()
-    if storage:contains('mt_teams:is_in_here_somewhere') == true then
-        mt_teams.load_teams()
-    else
-        mt_teams.save_teams()
-    end
     if meta:contains('mt_teams:team') == false then
         math.randomseed(100)
-        local rand = math.ceil((math.random()*mt_teams.teams_num))
+        local rand = math.ceil((math.random()*#mt_teams.teams))
         mt_teams.set_team(player, rand)
         minetest.chat_send_player(player:get_player_name(), mt_teams.teams[rand].name)
     end
