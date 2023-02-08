e49378729d1801367bbe98525ab17f058d9d8151
pl608 <computerprojects46@gmail.com>
2023-02-07 17:41:23 -0800

    filling in functions
    untested

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 mt_teams = {}
+local storage = minetest.get_mod_storage()
 mt_teams.colors ={
     black='#2b2b2b',
     blue='#0063b0',
@@ -16,23 +17,53 @@ mt_teams.colors ={
     white='#FFFFFF',
     yellow='#ffe400',
 }
+mt_teams.teams = {}
+mt_teams.teams_num = 0
 players = {}
 
-
+function mt_teams.load_teams()
+    
+end
 
 minetest.register_on_joinplayer(function(player, last_login)
-    local name_ = player:get_player_name()
+    mt_teams.set_team(player)
     math.randomseed()
-    players[name_] = mt_teams.colors[math.floor((math.random()*15))]
-    minetest.chat_send_all(string.format("*** %s joined the game.",
-					minetest.colorize(players[name_], name_))
-				)
+    local rand = math.floor((math.random()*mt_teams.teams_num))
+    mt_teams.set_teams(player, team)
 end
 )
 
 function mt_teams.get_team(player)
-
+    local meta = player:get_meta()
+    return meta:get_float("mt_teams:team")
+end
+function mt_teams.get_teams_name(player)
+    local meta = player:get_meta()
+    return meta:get_name("mt_teams:name")
+end
+function mt_teams.get_name_id(id)
+    return mt_teams.teams[id].name
 end
 function mt_teams.set_team(player,team)
-
+    local meta = player:get_meta()
+    if mt_teams.teams[team] ~= nil then
+        meta:set_float('team', team)
+        minetest.chat_send_all(string.format("*** %s joined team "..to_string(mt_teams.teams[team].name),
+    minetest.colorize(players[name_], name_)))
+    else
+        minetest.chat_send_player(player:get_name(), "Team doesnt appear to exist")
+    end
+end
+function mt_teams.create_team(player, name, color)
+    mt_teams.teams_num = mt_teams.teams_num+1
+    local meta = player:get_meta()
+    meta:set_float('mt_teams:team', mt_teams.teams_num)
+    meta:set_bool('mt_teams:is_owner', true)
+    meta:set_string('mt_teams:name', name)
+    table.insert_all(mt_teams.teams, 
+        {color=color,
+        owner=player,
+        name=name,
+        id=mt_teams.teams_num
+    })
 end
